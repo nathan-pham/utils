2d89a5b33bb9d519cc44c68807e2423e1da0382a
Nathan Pham <nathanpham.me@gmail.com>
2021-07-08 20:20:45 -0700

    auto export

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ const path = require("path")
 const fs = require("fs")
 
 const files = fs.readdirSync(__dirname)
-	.filter(file => (
-        file !== "index.js"
-    ))
+	.filter(file => (file !== "index.js"))
 	.map(file => ({ 
         namespace: file.split('.').shift(), 
         exports: require(path.join(__dirname, file)) 
